ef3e39ec2a9ad05a508038271269e601232f320b
Nicholas Finch <ngf4@nau.edu>
2016-07-01 16:10:28 -0700

    Update: 2016-07-01_0410 PM

---
+++ gitter.bat
@@ -0,0 +1,8 @@
+:top
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+C:\Users\nicholas\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe add -A
+C:\Users\nicholas\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe commit -m "Update: %mydate%_%mytime%"
+timeout /t 60
+goto :top
