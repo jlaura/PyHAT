df7dd2618624dad85c44cc19aff1536599c5ca4b
Nicholas Finch <ngf4@nau.edu>
2016-07-18 13:27:31 -0700

    Update: 2016-07-18_0127 PM

--- build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/out.py
+++ build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/out.py
@@ -1,3 +1,7 @@
 from pysat.spectral.spectral_data import spectral_data
 from pysat.regression.pls_sm import pls_sm
 import pandas as pd
+maskfile = "C:/Users/nicholas/.bash_history"
+unknowndatacsv = "C:/Users/nicholas/.gitconfig"
+db = "C:/Users/nicholas/lifecynical.swf"
+outpath = "C:/Users/nicholas/VirtualBox VMs"
