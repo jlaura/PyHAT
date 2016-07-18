65f82b9d2067b48a9c678a6763f260173eca7ca4
Nicholas Finch <ngf4@nau.edu>
2016-07-18 11:28:13 -0700

    Update: 2016-07-18_1127 AM

---
+++ build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_guitest.cpp
@@ -0,0 +1,203 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'guitest.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../src/PYSAT_GUI/guitest.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'guitest.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_GuiTest_t {
+    QByteArrayData data[24];
+    char stringdata0[372];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_GuiTest_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_GuiTest_t qt_meta_stringdata_GuiTest = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "GuiTest"
+QT_MOC_LITERAL(1, 8, 15), // "setSizeOfWindow"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 17), // "setupQSpinWidgets"
+QT_MOC_LITERAL(4, 43, 12), // "setupOutFile"
+QT_MOC_LITERAL(5, 56, 20), // "setNormValuesVisible"
+QT_MOC_LITERAL(6, 77, 5), // "index"
+QT_MOC_LITERAL(7, 83, 7), // "visible"
+QT_MOC_LITERAL(8, 91, 21), // "on_toolButton_clicked"
+QT_MOC_LITERAL(9, 113, 23), // "on_toolButton_2_clicked"
+QT_MOC_LITERAL(10, 137, 23), // "on_toolButton_3_clicked"
+QT_MOC_LITERAL(11, 161, 23), // "on_toolButton_4_clicked"
+QT_MOC_LITERAL(12, 185, 23), // "on_actionExit_triggered"
+QT_MOC_LITERAL(13, 209, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(14, 234, 14), // "SpinBoxChanged"
+QT_MOC_LITERAL(15, 249, 8), // "QWidget*"
+QT_MOC_LITERAL(16, 258, 3), // "wSp"
+QT_MOC_LITERAL(17, 262, 12), // "spinboxWrite"
+QT_MOC_LITERAL(18, 275, 1), // "i"
+QT_MOC_LITERAL(19, 277, 23), // "on_toolButton_5_clicked"
+QT_MOC_LITERAL(20, 301, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(21, 323, 16), // "numberOfNewlines"
+QT_MOC_LITERAL(22, 340, 1), // "n"
+QT_MOC_LITERAL(23, 342, 29) // "on_lineEdit_5_editingFinished"
+
+    },
+    "GuiTest\0setSizeOfWindow\0\0setupQSpinWidgets\0"
+    "setupOutFile\0setNormValuesVisible\0"
+    "index\0visible\0on_toolButton_clicked\0"
+    "on_toolButton_2_clicked\0on_toolButton_3_clicked\0"
+    "on_toolButton_4_clicked\0on_actionExit_triggered\0"
+    "on_pushButton_13_clicked\0SpinBoxChanged\0"
+    "QWidget*\0wSp\0spinboxWrite\0i\0"
+    "on_toolButton_5_clicked\0on_pushButton_clicked\0"
+    "numberOfNewlines\0n\0on_lineEdit_5_editingFinished"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GuiTest[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    2,   97,    2, 0x08 /* Private */,
+       8,    0,  102,    2, 0x08 /* Private */,
+       9,    0,  103,    2, 0x08 /* Private */,
+      10,    0,  104,    2, 0x08 /* Private */,
+      11,    0,  105,    2, 0x08 /* Private */,
+      12,    0,  106,    2, 0x08 /* Private */,
+      13,    0,  107,    2, 0x08 /* Private */,
+      14,    1,  108,    2, 0x08 /* Private */,
+      17,    1,  111,    2, 0x08 /* Private */,
+      19,    0,  114,    2, 0x08 /* Private */,
+      20,    0,  115,    2, 0x08 /* Private */,
+      21,    1,  116,    2, 0x08 /* Private */,
+      23,    0,  119,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool,    6,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Int, 0x80000000 | 15,   16,
+    QMetaType::Void, 0x80000000 | 15,   18,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   22,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void GuiTest::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        GuiTest *_t = static_cast<GuiTest *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->setSizeOfWindow(); break;
+        case 1: _t->setupQSpinWidgets(); break;
+        case 2: _t->setupOutFile(); break;
+        case 3: _t->setNormValuesVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 4: _t->on_toolButton_clicked(); break;
+        case 5: _t->on_toolButton_2_clicked(); break;
+        case 6: _t->on_toolButton_3_clicked(); break;
+        case 7: _t->on_toolButton_4_clicked(); break;
+        case 8: _t->on_actionExit_triggered(); break;
+        case 9: _t->on_pushButton_13_clicked(); break;
+        case 10: { int _r = _t->SpinBoxChanged((*reinterpret_cast< QWidget*(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
+        case 11: _t->spinboxWrite((*reinterpret_cast< QWidget*(*)>(_a[1]))); break;
+        case 12: _t->on_toolButton_5_clicked(); break;
+        case 13: _t->on_pushButton_clicked(); break;
+        case 14: _t->numberOfNewlines((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->on_lineEdit_5_editingFinished(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 10:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QWidget* >(); break;
+            }
+            break;
+        case 11:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QWidget* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject GuiTest::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_GuiTest.data,
+      qt_meta_data_GuiTest,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *GuiTest::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GuiTest::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_GuiTest.stringdata0))
+        return static_cast<void*>(const_cast< GuiTest*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int GuiTest::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/out.py
@@ -0,0 +1,3 @@
+from pysat.spectral.spectral_data import spectral_data
+from pysat.regression.pls_sm import pls_sm
+import pandas as pd
