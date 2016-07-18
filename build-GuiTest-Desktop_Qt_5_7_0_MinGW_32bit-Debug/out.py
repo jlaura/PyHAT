bee8ad6315df1d8d99a518dff7a438f145cb4843
Nicholas Finch <ngf4@nau.edu>
2016-07-18 15:20:30 -0700

    Update: 2016-07-18_0320 PM

--- build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/out.py
+++ build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/out.py
@@ -1,4 +1,7 @@
 from pysat.spectral.spectral_data import spectral_data
 from pysat.regression.pls_sm import pls_sm
 import pandas as pd
-maskfile = ""
+maskfile = "C:/Users/nicholas/Desktop/mask_minors_noise.csv"
+unknowndatacsv = "C:/Users/nicholas/Desktop/lab_data_averages_pandas_format.csv"
+db = "C:/Users/nicholas/Desktop/full_db_mars_corrected_dopedTiO2_pandas_format.csv"
+outpath = "C:/Users/nicholas/Desktop/Output"
