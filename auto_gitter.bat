9d3b4a785fafc1d5f7e7ee4e49acfeef008aa17d
Nicholas Finch <ngf4@nau.edu>
2016-08-08 16:15:33 -0700

    Update: 2016-08-08_0415 PM

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -1,11 +1,11 @@
-%USERPROFILE%\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe config remote.origin.url git@github.com:tisaconundrum2/PySAT.git
+%USERPROFILE%\AppData\Local\GitHub\PortableGit_624c8416ee51e205b3f892d1d904e06e6f3c57c8\cmd\git.exe config remote.origin.url git@github.com:tisaconundrum2/PySAT.git
 :top
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-%USERPROFILE%\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe pull origin master
-%USERPROFILE%\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe add -A
-%USERPROFILE%\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe commit -m "Update: %mydate%_%mytime%"
-%USERPROFILE%\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd\git.exe push -u origin master
+%USERPROFILE%\AppData\Local\GitHub\PortableGit_624c8416ee51e205b3f892d1d904e06e6f3c57c8\cmd\git.exe pull origin master
+%USERPROFILE%\AppData\Local\GitHub\PortableGit_624c8416ee51e205b3f892d1d904e06e6f3c57c8\cmd\git.exe add -A
+%USERPROFILE%\AppData\Local\GitHub\PortableGit_624c8416ee51e205b3f892d1d904e06e6f3c57c8\cmd\git.exe commit -m "Update: %mydate%_%mytime%"
+%USERPROFILE%\AppData\Local\GitHub\PortableGit_624c8416ee51e205b3f892d1d904e06e6f3c57c8\cmd\git.exe push -u origin master
 timeout /t 600
 goto :top
