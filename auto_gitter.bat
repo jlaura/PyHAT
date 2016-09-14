e110dd39527ef46d42edb50e77d60249f3db6af4
Nicholas Finch <ngf4@nau.edu>
2016-09-14 11:12:21 -0700

    Auto Generated Update:_

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -3,11 +3,9 @@
 for /r %USERPROFILE% %%I in (*git.exe) do (
 
 	%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT.git
-	For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-	For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 	%%I pull origin master
 	%%I add -A
-	%%I commit -m "Update: %mydate%_%mytime%"
+	%%I commit -m "Auto Generated Update:_
 	%%I push -u origin master
 	timeout /t 600
 )
