467463efa289110293fd7ecf5865f03d2c047a2f
Nicholas Finch <ngf4@nau.edu>
2016-09-28 10:05:46 -0700

    Fixing Auto Gitter one more time

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -2,10 +2,10 @@
 Setlocal EnableDelayedExpansion
 :top
 for /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
-	set /p "commit= commit -m: "
 	%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT.git
 	%%I pull origin master
 	%%I add -A
+	set /p "commit= commit -m: "
 	%%I commit -m "!commit!"
 	%%I push -u origin master
 )
