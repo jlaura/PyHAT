9fc2305808b8451a5ee086155497575b5abe9298
Ryan Anderson <rbanderson@usgs.gov>
2016-09-30 11:13:26 -0700

    Temporary tweaks to make everything work nicely for now

--- pysat/spectral/within_range.py
+++ pysat/spectral/within_range.py
@@ -4,6 +4,10 @@ Created on Tue May 10 16:11:37 2016
 
 @author: rbanderson
 """
-def within_range(x,y,rangevals):
-    mask=(y>rangevals[0])&(y<rangevals[1])
-    return x.loc[mask],y.loc[mask]
+#def within_range(x,y,rangevals):
+    #mask=(y>rangevals[0])&(y<rangevals[1])
+    #return x.loc[mask],y.loc[mask]
+    
+def within_range(data,rangevals,col):
+    mask=(data[('meta',col)]>rangevals[0])&(data[('meta',col)]<rangevals[1])
+    return data.loc[mask]
