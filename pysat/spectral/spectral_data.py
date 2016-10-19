aa1714f0fac2a08b0b4f8f6a271f955915d7ea87
Ryan Anderson <rbanderson@usgs.gov>
2016-10-19 11:52:04 -0700

    Got CCAM baseline removal implemented in spectral_data

--- pysat/spectral/spectral_data.py
+++ pysat/spectral/spectral_data.py
@@ -20,6 +20,7 @@ from pysat.spectral.baseline_code.mario import Mario
 from pysat.spectral.baseline_code.median import MedianFilter
 from pysat.spectral.baseline_code.rubberband import Rubberband
 from pysat.spectral.jade import jadeR as jade
+from pysat.spectral.baseline_code.ccam_remove_continuum import ccam_br
 
 def norm_total(df):
     df=df.div(df.sum(axis=1),axis=0)
@@ -200,18 +201,22 @@ class spectral_data(object):
             br=MedianFilter()
         if method is 'rubberband':
             br=Rubberband()
+        if method is 'ccam':
+            br=ccam_br()
         #if method is 'wavelet':
          #   br=Wavelet()
             
             
         #if parameters are provided, use them to set the parameters of br
         if params is not None:
-            for i in br.__dict__.keys():
+            for i in params.keys():
                 try:
-                    br[i]=params[i]
+                    setattr(br,i,params[i])
                 except:
                     print('Required keys are:')
                     print(br.__dict__.keys())
+                    print('Exiting without removing baseline!')
+                    return
         br.fit(wvls,spectra,segment=segment)
         self.df_baseline=self.df.copy()
         self.df_baseline['wvl']=br.baseline
