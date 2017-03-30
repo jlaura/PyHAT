2039cb417ef8a976a711666bb7d45a6c2c017e08
Nicholas Finch <ngf4@nau.edu>
2017-03-30 09:35:20 -0700

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+
+)
+
+setup(name="PySAT_Point_Spectra_GUI",
+      version="1.3",
+      description="A PDART-funded effort to design a spectral analysis tool for LIBS (and other) spectra",
+      url="https://github.com/USGS-Astrogeology/PySAT",
+      author="Ryan B. Anderson, Jay Laura, Nicholas Finch",
+      author_email='rbanderson@usgs.gov',
+      license='MIT',
+      packages=['PYSAT'],
+      zip_safe=False)
