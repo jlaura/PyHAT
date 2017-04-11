ce87d29571b7176e4fb356e79a29f7f9e605acb6
Nicholas Finch <ngf4@nau.edu>
2017-04-11 14:24:27 -0700

    Successfully got setup.py working
    Package can now be used in tangent with point_spectra_gui library

---
+++ setup.py
@@ -0,0 +1,71 @@
+from setuptools import setup, find_packages
+from os import path
+import pysat
+
+VERSION = pysat.__version__
+here = path.abspath(path.dirname(__file__))
+
+# TODO PyPi requires a README.rst file, not a README.md
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+setup(
+    name="PYSAT",
+    version=VERSION,
+    author="Jay Laura",
+    author_email="jlaura@usgs.gov",
+    description="TODO Add description...",
+    # long_description=long_description,
+    url="https://github.com/USGS-Astrogeology/PySAT",
+    license="Public Domain",
+    extras_requires=[
+        'copy',
+        'cvxopt',
+        'fnmatch',
+        'functools',
+        'glob',
+        'itertools',
+        'json',
+        'matplotlib',
+        'numpy',
+        'osgeo',
+        'pandas',
+        'pickle',
+        'pvl',
+        'PyQt4',
+        'pysat',
+        'pywt',
+        'scipy',
+        'shutil',
+        'sklearn',
+        'tempfile',
+        'time',
+        'unittest',
+        'warnings',
+        'yaml'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Spectral Analysis",
+        "License :: Public Domain",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords="planetary io",
+    packages=(
+        'pysat',
+        'pysat.examples',
+        'pysat.fileio',
+        'pysat.fileio.tests',
+        'pysat.plotting',
+        'pysat.regression',
+        'pysat.spectral',
+        'pysat.spectral.baseline_code',
+        'pysat.spectral.tests',
+        'pysat.utils',
+        'pysat.utils.tests'
+    ),
+)
