413e43c971026131e8d8e6d111ed5d1001e8d139
tisaconundrum2 <ngf4@nau.edu>
2017-07-21 21:15:40 -0700

    Adding time into auto_gitter

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -1,11 +1,13 @@
 @echo off
 :top
-for /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
-	%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT.git
-	%%I pull origin dev
-	%%I add -A
-	%%I commit -m "Auto Generated Update"
-	%%I push -u origin dev
-	timeout /t 600
+
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+FOR /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
+%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT.git
+%%I  add -A
+%%I  commit -m "Update %mydate%_%mytime%"
+timeout /t 60
 )
 goto :top
