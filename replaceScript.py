167114100ab63e5197aaebc1d90bbca03dfb4f4a
Nicholas Finch <ngf4@nau.edu>
2017-10-05 17:57:08 -0700

    add replace

---
+++ replaceScript.py
@@ -0,0 +1,13 @@
+import glob
+
+listOfFiles = []
+for filename in glob.iglob('**/*.py', recursive=True):
+    with open(filename, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace('', '')
+
+    # Write the file out again
+    with open(filename, 'w') as file:
+        file.write(filedata)
