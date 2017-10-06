a91258a4dc099476b64f9a52b7b3f4817602e7fc
Nicholas Finch <ngf4@nau.edu>
2017-10-05 18:42:12 -0700

    use this to search for code

--- replaceScript.py
+++ replaceScript.py
@@ -11,3 +11,18 @@ for filename in glob.iglob('**/*.py', recursive=True):
     # Write the file out again
     with open(filename, 'w') as file:
         file.write(filedata)
+
+import os
+
+user_input = './'
+directory = os.listdir(user_input)
+
+searchstring = input('What word are you trying to find?')
+
+for filename in glob.iglob('**/*.py', recursive=True):
+    with open(filename, 'r') as file:
+        filedata = file.read()
+
+        if searchstring in filedata:
+            print('found string in file %s' % filename)
+        file.close()
