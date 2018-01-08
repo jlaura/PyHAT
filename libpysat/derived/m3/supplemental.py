d6c26877c3acc7552107e1fbc6c7170f6fb6ef81
Adam Paquette <acp263@nau.edu>
2018-01-08 13:23:25 -0700

    Updated generic to generic_func due to issues with numpy generic import.

--- libpysat/derived/m3/supplemental.py
+++ libpysat/derived/m3/supplemental.py
@@ -1,4 +1,4 @@
-from libpysat.utils.utils import generic, getbandnumbers
+from libpysat.utils.utils import generic_func, getbandnumbers
 from libpysat.derived.m3 import supplemental_funcs as sp_funcs
 
 #TODO: The continuum in these funcs should default to linear
@@ -34,7 +34,7 @@ def curvature(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [730, 749, 909, 1109, 1129]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.curv_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.curv_func)
 
 def fe_est(data, wv_array, continuum = None, continuum_args = ()):
     '''
@@ -70,7 +70,7 @@ def fe_est(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [730, 749, 949, 970]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.fe_est_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.fe_est_func)
 
 def fe_mare_est(data, wv_array, continuum = None, continuum_args = ()):
     '''
@@ -103,7 +103,7 @@ def fe_mare_est(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [730, 749, 949, 970]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.fe_mare_est_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.fe_mare_est_func)
 
 def luceyc_amat(data, wv_array, continuum = None, continuum_args = ()):
     '''
@@ -136,7 +136,7 @@ def luceyc_amat(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [730, 749, 949, 970]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.luceyc_amat_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.luceyc_amat_func)
 
 def luceyc_omat(data, wv_array, continuum = None, continuum_args = ()):
     '''
@@ -169,7 +169,7 @@ def luceyc_omat(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [730, 749, 949, 970]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.luceyc_omat_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.luceyc_omat_func)
 
 def mare_omat(data, wv_array, continuum = None, continuum_args = ()):
     '''
@@ -202,7 +202,7 @@ def mare_omat(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [730, 749, 949, 970]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.mare_omat_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.mare_omat_func)
 
 def tilt(data, wv_array, continuum = None, continuum_args = ()):
     '''
@@ -234,4 +234,4 @@ def tilt(data, wv_array, continuum = None, continuum_args = ()):
     wavelengths = [909, 930, 1009, 1029]
     if continuum:
         continuum(data, wavelengths, continuum, continuum_args)
-    return generic(data, wv_array, wavelengths, func = sp_funcs.tilt_func)
+    return generic_func(data, wv_array, wavelengths, func = sp_funcs.tilt_func)
