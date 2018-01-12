de32e30735bf87941b5da0abce9dd607d06b5331
Kelvin Rodriguez <kelvinrr@icloud.com>
2018-01-12 02:15:02 -0700

    tests4dayz

--- tests/derived/m3/test_supplemental.py
+++ tests/derived/m3/test_supplemental.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 
 from libpysat.derived.m3 import supplemental as sup
-from libpysat.spectral.spectra import Spectra
+from libpysat import HCube
 
 @pytest.fixture
 def test_params(wavelengths):
@@ -12,7 +12,7 @@ def test_params(wavelengths):
                           [test_params([749, 909, 1109])])
 
 def test_curvature(three_dim, wv_array, expected):
-    data = Spectra(three_dim, wv_array, waxis = 0)
+    data = HCube(three_dim, wv_array, waxis = 0)
     res = sup.curvature(data, wv_array)
     assert res.all() == expected
 
@@ -20,7 +20,7 @@ def test_curvature(three_dim, wv_array, expected):
                           [test_params([749, 949])])
 
 def test_fe_est(two_dim, wv_array, expected):
-    data = Spectra(two_dim, wv_array, waxis = 0)
+    data = HCube(two_dim, wv_array, waxis = 0)
     res = sup.fe_est(data, wv_array)
     assert res.all() == expected
 
@@ -28,7 +28,7 @@ def test_fe_est(two_dim, wv_array, expected):
                           [test_params([749, 949])])
 
 def test_fe_mare_est(two_dim, wv_array, expected):
-    data = Spectra(two_dim, wv_array, waxis = 0)
+    data = HCube(two_dim, wv_array, waxis = 0)
     res = sup.fe_mare_est(data, wv_array)
     assert res.all() == expected
 
@@ -36,7 +36,7 @@ def test_fe_mare_est(two_dim, wv_array, expected):
                           [test_params([749, 949])])
 
 def test_luceyc_amat(two_dim, wv_array, expected):
-    data = Spectra(two_dim, wv_array, waxis = 0)
+    data = HCube(two_dim, wv_array, waxis = 0)
     res = sup.luceyc_amat(data, wv_array)
     assert res.all() == expected
 
@@ -44,7 +44,7 @@ def test_luceyc_amat(two_dim, wv_array, expected):
                           [test_params([749, 949])])
 
 def test_luceyc_omat(two_dim, wv_array, expected):
-    data = Spectra(two_dim, wv_array, waxis = 0)
+    data = HCube(two_dim, wv_array, waxis = 0)
     res = sup.luceyc_omat(data, wv_array)
     assert res.all() == expected
 
@@ -52,7 +52,7 @@ def test_luceyc_omat(two_dim, wv_array, expected):
                           [test_params([749, 949])])
 
 def test_mare_omat(two_dim, wv_array, expected):
-    data = Spectra(two_dim, wv_array, waxis = 0)
+    data = HCube(two_dim, wv_array, waxis = 0)
     res = sup.mare_omat(data, wv_array)
     assert res.all() == expected
 
@@ -60,6 +60,6 @@ def test_mare_omat(two_dim, wv_array, expected):
                           [test_params([930, 1009])])
 
 def test_tilt(two_dim, wv_array, expected):
-    data = Spectra(two_dim, wv_array, waxis = 0)
+    data = HCube(two_dim, wv_array, waxis = 0)
     res = sup.tilt(data, wv_array)
     assert res.all() == expected
