f13352a03cfddb882bfbaf0cb4bb19213800abf5
Ryan Anderson <rbanderson@usgs.gov>
2018-02-27 08:56:30 -0700

    Leverage built-in CV methods
    I'm commiting this, but I think these methods, while efficient, hide too
    much of the CV information from the user. I am going to try a different
    approach, using the underlying path calculations so that we can still
    get all the details of the CV run, while still gaining efficiency.

--- libpysat/regression/cv.py
+++ libpysat/regression/cv.py
@@ -15,6 +15,7 @@ import pandas as pd
 from libpysat.regression.regression import regression
 from sklearn.cross_validation import LeaveOneLabelOut
 from sklearn.grid_search import ParameterGrid
+from sklearn.linear_model import ElasticNetCV, LassoCV, RidgeCV, OrthogonalMatchingPursuitCV, LarsCV, LassoLarsCV
 warnings.filterwarnings('ignore')
 
 
@@ -30,7 +31,7 @@ class cv:
         self.paramgrid = list(ParameterGrid(params))  # create a grid of parameter permutations
         #self.paramgrid = ParameterGrid(params).param_grid
     def do_cv(self, Train, xcols='wvl', ycol=('comp', 'SiO2'), method='PLS',
-              yrange=[0, 100]):
+              yrange=[0, 100], calc_path = False, alphas = None, l1_ratios = None):
 
 
         try:
@@ -60,44 +61,95 @@ class cv:
             modelkey = "{} - {} - ({}, {}) {}".format(method, ycol[0][-1], yrange[0], yrange[1], self.paramgrid[i])
 
             rmsecv_folds_tmp = []  # Create empty list to hold RMSECV for each fold
-            for train, holdout in cv_iterator:  # Iterate through each of the folds in the training set
 
-                cvcol = ('predict', '"'+method + '-CV-' + str(self.paramgrid[
-                                                           i])+'"')  # ycol[-1]+'_cv_'+method+'_param'+str(i))  #create the name of the column in which results will be stored
+            #if possible, leverage sklearn efficient CV functions
+            if calc_path:
+                if method == 'Elastic Net':
+                    CV = ElasticNetCV(l1_ratio=l1_ratios, alphas=alphas, cv=cv_iterator, n_jobs = -1,
+                                          **self.paramgrid[i])
+                    CV.fit(Train[xcols],Train[ycol])
+                if method == 'LASSO':
+                    CV = LassoCV(alphas=alphas, cv=cv_iterator, n_jobs=-1, **self.paramgrid[i])
+                    CV.fit(Train[xcols],Train[ycol])
 
-                cv_train = Train.iloc[train]  # extract the data to be used to create the model
-                cv_holdout = Train.iloc[holdout]  # extract the data that will be held out of the model
-                model.fit(cv_train[xcols], cv_train[ycol])
-                if model.goodfit:
-                    y_pred_holdout = model.predict(cv_holdout[xcols])
-                else:
-                    y_pred_holdout = cv_holdout[ycol] * np.nan
-                Train.set_value(Train.index[holdout], cvcol, y_pred_holdout)
-                rmsecv_folds_tmp.append(RMSE(y_pred_holdout, cv_holdout[ycol]))
 
-            rmsecv_folds.append(rmsecv_folds_tmp)
-            rmsecv.append(RMSE(Train[ycol], Train[cvcol]))
+                for j in list(range(len(CV.mse_path_))):
+                    rmsecvs_tmp = pd.DataFrame(np.sqrt(CV.mse_path_[j]))
+                    rmsecvs_tmp['Average RMSECV'] = np.mean(np.sqrt(CV.mse_path_[j], axis=1))
+                    rmsecvs_tmp['alpha'] = CV.alphas_
+                    try:
+                        rmsecvs_tmp['l1_ratio'] = CV.l1_ratio[j]
+                    except:
+                        pass
+                    for key, value in self.paramgrid[i].items():
+                        rmsecvs_tmp[key] = value
+                    try:
+                        rmsecvs.concat(rmsecvs_tmp)
+                    except:
+                        rmsecvs = rmsecvs_tmp
 
-            model.fit(Train[xcols], Train[ycol])
-            if model.goodfit:
-                models.append(model)
-                modelkeys.append(modelkey)
-                ypred_train = model.predict(Train[xcols])
+                # if method == 'LARS':
+                #     pass
+                # if method == 'LASSO LARS':
+                #     pass
+                # if method == 'OMP':
+                #     pass
+                # if method == 'Ridge':
+                #     CV = RidgeCV(alphas = alphas, cv = cv_iterator, **self.paramgrid[i], store_cv_values=True)
+                #     CV.fit(Train[xcols],Train[ycol])
 
             else:
-                ypred_train = Train[ycol] * np.nan
-            calcol = ('predict', '"'+method + '-Cal-' + str(self.paramgrid[i])+'"')
-            Train[calcol] = ypred_train
-            rmsec.append(RMSE(ypred_train, Train[ycol]))
+                for train, holdout in cv_iterator:  # Iterate through each of the folds in the training set
+                    cvcol = ('predict', '"'+method + '-CV-' + str(self.paramgrid[
+                                                               i])+'"')  # ycol[-1]+'_cv_'+method+'_param'+str(i))  #create the name of the column in which results will be stored
 
-        output = pd.DataFrame(self.paramgrid)
-        output['RMSEC'] = rmsec
-        output['RMSECV'] = rmsecv
-        rmsecv_folds = np.array(rmsecv_folds)
-        for i in list(range(len(rmsecv_folds[0, :]))):
-            label = 'Fold' + str(i)
-            output[label] = rmsecv_folds[:, i]
-        cols = output.columns.values
-        cols = [('cv', i) for i in cols]
-        output.columns = pd.MultiIndex.from_tuples(cols)
-        return Train, output, models, modelkeys
+                    cv_train = Train.iloc[train]  # extract the data to be used to create the model
+                    cv_holdout = Train.iloc[holdout]  # extract the data that will be held out of the model
+                    #fit the model and predict teh held-out data
+                    model.fit(cv_train[xcols], cv_train[ycol])
+                    if model.goodfit:
+                        y_pred_holdout = model.predict(cv_holdout[xcols])
+                    else:
+                        y_pred_holdout = cv_holdout[ycol] * np.nan
+                    #add the predictions to the appropriate column in the training data
+                    Train.set_value(Train.index[holdout], cvcol, y_pred_holdout)
+                    #append the RMSECV to the list
+                    rmsecv_folds_tmp.append(RMSE(y_pred_holdout, cv_holdout[ycol]))
+
+                #append the RMSECVs for the current settings to the collection of all RMSECVs
+                rmsecv_folds.append(rmsecv_folds_tmp)
+                rmsecv.append(RMSE(Train[ycol], Train[cvcol]))
+
+                #fit the model on the full training set using the current settings
+                model.fit(Train[xcols], Train[ycol])
+                #if the fit is good, then predict the training set
+                if model.goodfit:
+                    models.append(model)
+                    modelkeys.append(modelkey)
+                    ypred_train = model.predict(Train[xcols])
+
+                else:
+                    ypred_train = Train[ycol] * np.nan
+
+                #add the calibration predictions to the appropriate column
+                calcol = ('predict', '"'+method + '-Cal-' + str(self.paramgrid[i])+'"')
+                Train[calcol] = ypred_train
+                #append the RMSEC for the current settings to the cllection of all RMSECs
+                rmsec.append(RMSE(ypred_train, Train[ycol]))
+
+                #create an output data frame from the grid of parameters
+                output = pd.DataFrame(self.paramgrid)
+
+                #add columns for RMSEC, RMSECV, and RMSECV for the folds
+                output['RMSEC'] = rmsec
+                output['RMSECV'] = rmsecv
+                rmsecv_folds = np.array(rmsecv_folds)
+                for i in list(range(len(rmsecv_folds[0, :]))):
+                    label = 'Fold' + str(i)
+                    output[label] = rmsecv_folds[:, i]
+                #make the columns of the output data drame multi-indexed
+                cols = output.columns.values
+                cols = [('cv', i) for i in cols]
+                output.columns = pd.MultiIndex.from_tuples(cols)
+
+                return Train, output, models, modelkeys
