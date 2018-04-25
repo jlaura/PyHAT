02484f1c271668102b05a2601628d9eda8fb1aca
Ryan <rbanderson@usgs.gov>
2018-04-24 21:31:35 -0500

    Trying to fix paths for tests

--- libpysat/fileio/tests/test_spectral_profiler.py
+++ libpysat/fileio/tests/test_spectral_profiler.py
@@ -5,7 +5,7 @@ from libpysat.fileio import io_spectral_profiler as sp
 
 class Test_Spectral_Profiler_IO(unittest.TestCase):
     def setUp(self):
-        self.examplefile = get_path('SP_2C_02_02358_S138_E3586.spc')
+        self.examplefile = get_path('SpectralProfiler/SP_2C_02_02358_S138_E3586.spc')
 
     def test_openspc(self):
         dataset = sp.Spectral_Profiler(self.examplefile)
--- libpysat/fileio/tests/test_structured_io.py
+++ libpysat/fileio/tests/test_structured_io.py
@@ -16,11 +16,11 @@ from libpysat.examples import get_path
 class TestYAML(unittest.TestCase):
     @unittest.skipIf(missing == True, 'YAML library not installed')
     def test_read(self):
-        d = io_yaml.read_yaml(get_path('logging.yaml'))
+        d = io_yaml.read_yaml(get_path('loggers/logging.yaml'))
         self.assertIn('handlers', d.keys())
 
 
 class TestJSON(unittest.TestCase):
     def test_read(self):
-        d = io_json.read_json(get_path('logging.json'))
+        d = io_json.read_json(get_path('loggers/logging.json'))
         self.assertIn('handlers', d.keys())
