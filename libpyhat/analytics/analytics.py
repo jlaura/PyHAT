a0cb6c856ffdc472c893cd1acd65ba57966005a3
Cole <34170587+cneubauerUSGS@users.noreply.github.com>
2018-12-20 19:21:52 -0700

    Expanded Horgan Parameters (#170)
    * Added analytics folder with anaylitics.py and tests
    
    * Changed documentation to more accurately reflect returns
    
    * Added shells for test_analytics.py
    
    * band_minima was not inclusive of upper bound. Fixed function to include upper bound and updated test
    
    * Updated other functions to be inclusive. Updated tests, tc now at 41%
    
    * removed mystery function. Add tests tc unknown
    
    * Added tests
    
    * moved unrelated code back to transform/utils
    
    * Trigger Travis. new analytics test should be at 100% tc
    
    * Merged upstream and changed functions to take numpy arrays
    
    * Added import to init
    
    * Try #2
    
    * Try #2
    
    * Fixed an issue where the upper end was not inclusive
    
    * This is a guess
    
    * Old code made it back in in rename
    
    * Removed more old libpysat
    
    * No longer a series, renamed confusing variable
    
    * Added an example notebook showing the Analytics functions in use
    
    * Expanded Horgan Parameters to work on either spectrum or spectra objects
    
    * Updated output
    
    * added tests for syntax sugar

--- libpyhat/analytics/analytics.py
+++ libpyhat/analytics/analytics.py
@@ -1,7 +1,10 @@
 import math
 
 import numpy as np
-from pandas import Series
+import pandas as pd
+from libpyhat.data.spectra import Spectra
+from libpyhat.data.spectrum import Spectrum
+
 
 def band_minima(spectrum, low_endmember=None, high_endmember=None):
     """
@@ -41,6 +44,7 @@ def band_minima(spectrum, low_endmember=None, high_endmember=None):
 
     return minidx, minvalue
 
+
 def band_center(spectrum, low_endmember=None, high_endmember=None, degree=3):
 
     if not low_endmember:
@@ -60,6 +64,7 @@ def band_center(spectrum, low_endmember=None, high_endmember=None, degree=3):
 
     return center, center_fit
 
+
 def band_area(spectrum, low_endmember=None, high_endmember=None):
     """
     Compute the area under the curve between two endpoints where the
@@ -75,6 +80,7 @@ def band_area(spectrum, low_endmember=None, high_endmember=None):
     sub_spectrum = spectrum[low_endmember:high_endmember]
     return np.trapz(np.where(sub_spectrum <= 1.0))
 
+
 def band_asymmetry(spectrum, low_endmember=None, high_endmember=None, degree=3):
     """
     Compute the asymmetry of an absorption feature as
@@ -82,7 +88,7 @@ def band_asymmetry(spectrum, low_endmember=None, high_endmember=None, degree=3):
 
     Parameters
     ----------
-    specturm : object
+    spectrum : object
 
     low_endmember : int
         Bottom end of wavelengths to be obversed
@@ -117,4 +123,77 @@ def band_asymmetry(spectrum, low_endmember=None, high_endmember=None, degree=3):
     area_right = band_area(sub_spectrum[center_idx:])
 
     asymmetry = abs((area_left - area_right) / (area_left + area_right))
-    return asymmetry[0]
+    return asymmetry
+
+
+def analytics_series(spectrum, func, low_endmember=None, high_endmember=None):
+    """
+    Expects a spectrum object and calls func on it. Used in run_analytics to run
+    functions across spectra
+
+
+    Parameters
+    ----------
+    spectrum : Spectrum object
+
+    func  : analytic functions
+
+    low_endmember : int
+        Bottom end of wavelengths to be obversed
+
+    high_endmember : int
+        Top end of wavelengths to be obversed
+
+    Returns
+    -------
+    func : function return
+    """
+
+    if low_endmember:
+        low_endmember = np.where(spectrum.data.index==low_endmember)[0]
+    if high_endmember:
+        high_endmember = np.where(spectrum.data.index==high_endmember)[0][0]
+
+    ret = func(spectrum.data.values, low_endmember, high_endmember)
+    if func is band_minima:
+        wavelengths = spectrum.data.index[ret[0]]
+        ret = wavelengths, ret[1]
+    elif func is band_center:
+        wavelengths = spectrum.data.index[ret[0][0]]
+        center_fit = pd.Series(ret[1], index=spectrum.data.index)
+        ret = (wavelengths, ret[0][1]), center_fit
+    return ret
+
+
+def run_analytics(obj, func, low_endmember=None, high_endmember=None):
+    """
+    Wrapper function to run any analytic func on a spectrum or spectra
+
+    Parameters
+    ----------
+    obj : Spectrum or Spectra object
+
+    func  : analytic functions
+
+    low_endmember : int
+        Bottom end of wavelengths to be obversed
+
+    high_endmember : int
+        Top end of wavelengths to be obversed
+
+    Returns
+    -------
+    func : function return
+    """
+
+    if type(obj) is Spectrum:
+        return analytics_series(obj, func, low_endmember, high_endmember)
+    else:
+        if func is band_center:
+            index = obj.index[0:269]
+            series = [analytics_series(obj[i], func, low_endmember, high_endmember) for i in obj.columns]
+            centers, center_fits = zip(*series)
+            return pd.Series(centers, index=obj.columns), pd.DataFrame(data = np.transpose(center_fits), columns = obj.columns, index = index)
+
+        else:
+            return pd.Series([analytics_series(obj[i], func, low_endmember, high_endmember) for i in obj.columns], index = obj.columns)
--- libpyhat/analytics/tests/test_analytics.py
+++ libpyhat/analytics/tests/test_analytics.py
@@ -3,7 +3,9 @@ import pytest
 import numpy as np
 from numpy.testing import assert_array_equal
 
+import libpyhat as phat
 from libpyhat.analytics import analytics
+from libpyhat.examples import get_path
 
 @pytest.fixture
 def setUp():
@@ -49,3 +51,70 @@ def test_band_area():
 def test_band_asymmetry(spectrum, expected_val):
     assymetry = analytics.band_asymmetry(spectrum)
     assert assymetry == pytest.approx(expected_val)
+
+
+@pytest.mark.parametrize("expected_val", [(14715.763)]
+)
+def test_run_analytics_band_area(expected_val):
+    spectra = phat.Spectra.from_file(get_path('SP_2C_02_02358_S138_E3586.spc'))
+    area = analytics.run_analytics(spectra, analytics.band_area)
+    assert area.mean() == pytest.approx(expected_val)
+
+
+@pytest.mark.parametrize("expected_wavelengths, expected_values", [(2256.103,
+                                                                    909.947)]
+)
+def test_run_analytics_band_minima(expected_wavelengths, expected_values):
+    spectra = phat.Spectra.from_file(get_path('SP_2C_02_02358_S138_E3586.spc'))
+    minima = analytics.run_analytics(spectra, analytics.band_minima)
+    wavelengths = [np.mean(val[0]) for val in minima]
+    values = [val[1] for val in minima]
+    assert np.mean(wavelengths) == pytest.approx(expected_wavelengths)
+    assert np.mean(values) == pytest.approx(expected_values)
+
+
+@pytest.mark.parametrize("expected_center, expected_wavelengths, expected_values", [(2358.452,
+                                                                                     2256.103,
+                                                                                     909.947)]
+)
+def test_run_analytics_band_center(expected_center, expected_wavelengths, expected_values):
+    spectra = phat.Spectra.from_file(get_path('SP_2C_02_02358_S138_E3586.spc'))
+    center, center_fit = analytics.run_analytics(spectra, analytics.band_center)
+    wavelengths = [np.mean(val[0]) for val in center]
+    values = [np.mean(val[1]) for val in center]
+    # print(center_fit)
+    center_fit = [center_fit[spectrum] for spectrum in center_fit]
+    assert np.mean(center_fit) == pytest.approx(expected_center)
+    assert np.mean(wavelengths) == pytest.approx(expected_wavelengths)
+    assert np.mean(values) == pytest.approx(expected_values)
+
+
+@pytest.mark.parametrize("expected_center, expected_wavelengths, expected_values", [(0.1391204,
+                                                                                     2563.98,
+                                                                                     0.0)]
+)
+def test_run_analytics_band_center_spectrum(expected_center, expected_wavelengths, expected_values):
+    spectra = phat.Spectra.from_file(get_path('SP_2C_02_02358_S138_E3586.spc'))
+    spectrum = spectra[spectra.columns[1]]
+    center, center_fit = analytics.run_analytics(spectrum, analytics.band_center, 512.6, 2587.9)
+    assert center_fit.mean() == pytest.approx(expected_center)
+    assert np.mean(center[0]) == pytest.approx(expected_wavelengths)
+    assert np.mean(center[1]) == expected_values
+
+
+@pytest.mark.parametrize("expected_values", [(0.99974371)]
+)
+def test_run_analytics_band_asymmetry_spectrum(expected_values):
+    spectra = phat.Spectra.from_file(get_path('SP_2C_02_02358_S138_E3586.spc'))
+    spectrum = spectra[spectra.columns[1]]
+    asymmetry = analytics.run_analytics(spectrum, analytics.band_asymmetry, 512.6, 2587.9)
+    assert asymmetry == pytest.approx(expected_values)
+
+
+@pytest.mark.parametrize("expected_values", [(35912)]
+)
+def test_run_analytics_band_area_spectrum(expected_values):
+    spectra = phat.Spectra.from_file(get_path('SP_2C_02_02358_S138_E3586.spc'))
+    spectrum = spectra[spectra.columns[1]]
+    asymmetry = analytics.run_analytics(spectrum, analytics.band_area, 512.6, 2587.9)
+    assert asymmetry == pytest.approx(expected_values)
