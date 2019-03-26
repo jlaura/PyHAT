9748ad55d12c72efff229e9e428d0e51b62eb668
Ryan <rbanderson@usgs.gov>
2019-03-26 16:05:07 -0700

    Fix import for JADE

--- libpyhat/transform/dim_red.py
+++ libpyhat/transform/dim_red.py
@@ -1,4 +1,4 @@
-from libpyhat.transform.jade import JADE
+from bin.jade import JADE
 from sklearn.decomposition import PCA, FastICA
 from sklearn.manifold.t_sne import TSNE
 from sklearn.manifold.locally_linear import LocallyLinearEmbedding
