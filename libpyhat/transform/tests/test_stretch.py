4a2558c3728ea3753f8fae42f734b2770db75987
Ryan <rbanderson@usgs.gov>
2019-04-04 09:41:00 -0400

    Fixing fixture errors with test_stretch

--- libpyhat/transform/tests/test_stretch.py
+++ libpyhat/transform/tests/test_stretch.py
@@ -1,17 +1,22 @@
 from libpyhat.transform import stretch
+import numpy as np
 
-def test_linear_stretch(one_dim, expected = 1):
-    res = stretch.linear_stretch(one_dim)
+def one_dim():
+    return np.repeat(np.arange(1, 1 + 2), (25)).reshape(1,-1,5)
+
+
+def test_linear_stretch(expected = 1):
+    res = stretch.linear_stretch(one_dim())
     assert res.all() == expected
 
-def test_standard_deviation_stretch(one_dim, expected = 1):
-    res = stretch.standard_deviation_stretch(one_dim)
+def test_standard_deviation_stretch(expected = 1):
+    res = stretch.standard_deviation_stretch(one_dim())
     assert res.all() == expected
 
-def test_inverse_stretch(one_dim, expected = 0):
-    res = stretch.inverse_stretch(one_dim)
+def test_inverse_stretch(expected = 0):
+    res = stretch.inverse_stretch(one_dim())
     assert res.all() == expected
 
-def test_histequ_stretch(one_dim, expected = 1):
-    res = stretch.histequ_stretch(one_dim)
+def test_histequ_stretch(expected = 1):
+    res = stretch.histequ_stretch(one_dim())
     assert res.all() == expected
