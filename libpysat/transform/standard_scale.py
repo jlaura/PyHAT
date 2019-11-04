03c6e718957328f7fb6bdd6192fca205010c6376
Tyler Thatcher <tdt62@nau.edu>
2019-11-04 14:55:32 -0700

    Deleted PySAT files

--- libpysat/transform/standard_scale.py
+++
@@ -1,4 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-#apply standard scaling to a spectrum (mean center and divide each bin by the standard deviation of that bin)
-def standard_scale(self, col):
-    self.df[col] = StandardScaler().fit_transform(self.df[col])
