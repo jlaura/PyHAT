0f9a4841e895a9a9ce62d288d4cca9673ff0efe9
Tyler Thatcher <tdt62@nau.edu>
2019-11-25 14:39:32 -0700

    Added last files, and updated code for GUI

--- libpyhat/utils/folds.py
+++ libpyhat/utils/folds.py
@@ -1,36 +1,18 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
-from sklearn import cross_validation
+from sklearn import model_selection
 
 #This function assigns spectra to folds randomly, but keeps spectra with the same value in a user-defined column together.
 #This ensures that multiple spectra of the same target end up in the same fold
 
-def random(df, nfolds=5, seed=10, groupby=None):
+def random(df, nfolds=5, seed=10):
     df['Folds'] = 'None'  # Create an entry in the data frame that holds the folds
     foldslist = np.array(df['Folds'])
-    if groupby == None:  # if no column name is listed to group on, just create random folds
-        n = len(df.index)
-        folds = cross_validation.KFold(n, nfolds, shuffle=True, random_state=seed)
-        i = 1
-        for train, test in folds:
-            foldslist[test] = 'Fold' + str(i)
-            i = i + 1
-
-    else:
-        # if a column name is provided, get all the unique values and define folds
-        # so that all rows of a given value fall in the same fold
-        # (this is useful to ensure that training and test data are truly independent)
-        unique_inds = np.unique(df[groupby])
-        folds = cross_validation.KFold(len(unique_inds), nfolds, shuffle=True, random_state=seed)
-        foldslist = np.array(df['Folds'])
-        i = 1
-        for train, test in folds:
-            tmp = unique_inds[test]
-            tmp_full_list = np.array(df[groupby])
-            tmp_ind = np.in1d(tmp_full_list, tmp)
-            foldslist[tmp_ind] = 'Fold' + str(i)
-            i = i + 1
+    n = len(df.index)
+    folds = model_selection.KFold(n_splits=nfolds, shuffle=True, random_state=seed)
+    i = 1
+    for train, test in folds:
+        foldslist[test] = 'Fold' + str(i)
+        i = i + 1
 
     df['Folds'] = foldslist
     return df
@@ -56,5 +38,4 @@ def stratified_folds(df, nfolds=5, sortby=None):
 
     # sort by index to return the df to its original order
     df.sort_index(inplace=True)
-
     return df
