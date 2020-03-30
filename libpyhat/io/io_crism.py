5ccf354a7d66f1cd9855304041cbcfc3ca90b4e4
Ryan <rbanderson@usgs.gov>
2020-03-30 14:34:05 -0700

    Clean up CRISM and M3 io

--- libpyhat/io/io_crism.py
+++ libpyhat/io/io_crism.py
@@ -12,15 +12,12 @@ class Crism(GeoDataset, HCube):
     """
     An Crism specific reader with the spectral mixin.
     """
-    def __init__(self, file_name, libpyhat_enabled = True):
+    def __init__(self, file_name):
 
         GeoDataset.__init__(self, file_name)
         HCube.__init__(self)
 
-        self.derived_funcs = {}
-
-        if libpyhat_enabled:
-            self.derived_funcs = get_derived_funcs(crism)
+        self.derived_funcs = get_derived_funcs(crism)
 
     def __getattr__(self, name):
         try:
