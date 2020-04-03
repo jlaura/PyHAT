be60ce4a58afed136f141ac8c19ec0271ebc85f3
Thatcher <tthatcher84@gmail.com>
2020-04-03 09:42:33 -0700

    Update to deployment

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,3 +1,4 @@
 conda deactivate;
 conda config --set anaconda_upload yes;
+Write-Host "Python Version: $args[ $1 ]"
 conda build --token %CONDA_UPLOAD_TOKEN% --python $args[ $1 ] -c usgs-astrogeology recipe;
